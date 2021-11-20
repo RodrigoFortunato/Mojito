fc6aeee2391cba3b9b0260a4bdd64aa2ed5398b5
Rodrigo Fortunato <rodrigo.fortunato@wooza.com.br>
2021-11-20 11:28:50 -0300

    second commited

---
+++ src/Mojito.Business/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Mojito.Business.Models
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+    }
+}
